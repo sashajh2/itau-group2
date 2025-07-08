37ed9a1fc0e913967caf4c33f00833a85a24c1cc
Sean-Winston Luo <sean-winstonluo@31-37-208.wireless.csail.mit.edu>
2025-07-08 15:25:52 -0400

    arguments for triplet datasets

--- model_utils/models/siamese.py
+++ model_utils/models/siamese.py
@@ -20,8 +20,11 @@ class SiameseCLIPTriplet(BaseSiameseCLIP):
     """
     def __init__(self, embedding_dim=512, projection_dim=128, freeze_clip=True, backbone=None, tokenizer=None):
         super().__init__(embedding_dim, projection_dim, freeze_clip, backbone, tokenizer)
-    def forward(self, anchor_texts, positive_texts, negative_texts):
+    def forward(self, anchor_texts, positive_texts=None, negative_texts=None):
         z_anchor = self.encode(anchor_texts)
+        if positive_texts is None and negative_texts is None:
+            # Inference mode: only anchor embeddings needed
+            return z_anchor
         z_positive = self.encode(positive_texts)
         z_negative = self.encode(negative_texts)
         return z_anchor, z_positive, z_negative
