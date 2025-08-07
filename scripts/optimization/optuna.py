a901170d7ecc12c6017cec9dc6be4b75ab19eebe
Sean-Winston Luo <sean-winstonluo@dhcp-10-31-168-241.dyn.MIT.EDU>
2025-08-06 20:15:36 -0400

    reverting batch size

--- scripts/optimization/optuna.py
+++ scripts/optimization/optuna.py
@@ -43,7 +43,7 @@ class OptunaOptimizer(BaseOptimizer):
         """
         # Suggest hyperparameters
         lr = trial.suggest_float("lr", 1e-5, 1e-4, log=True)
-        batch_size = trial.suggest_categorical("batch_size", [64, 96, 128, 256])
+        batch_size = trial.suggest_categorical("batch_size", [64])
         internal_layer_size = trial.suggest_categorical("internal_layer_size", [512, 768, 1024])
         
 
